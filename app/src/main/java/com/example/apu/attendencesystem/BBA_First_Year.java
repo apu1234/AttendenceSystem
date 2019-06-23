db37a597854a20bf76fb9435b47dd5a2181f824a
dip9676 <dip9676@gmail.com>
2019-06-23 16:56:10 +0600

    I have done connecting activities.

---
+++ app/src/main/java/com/example/apu/attendencesystem/BBA_First_Year.java
@@ -0,0 +1,13 @@
+package com.example.apu.attendencesystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BBA_First_Year extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bba__first__year);
+    }
+}
---
+++ app/src/main/java/com/example/apu/attendencesystem/Civil_Third_Year.java
@@ -0,0 +1,13 @@
+package com.example.apu.attendencesystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Civil_Third_Year extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_civil__third__year);
+    }
+}
